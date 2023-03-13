aeed6bba92dfd3c2d24c12d4618a11f57c4e8de6
PrasadSMN <rpsoftwaretesting49@gmail.com>
2023-03-13 20:31:59 +0530

    13-03-2023 Programs

---
+++ prasadJava/Condition1.java
@@ -0,0 +1,25 @@
+package com.javaprograms;
+
+import java.util.Scanner;
+
+public class Condition1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the Number");
+		
+		int num=sc.nextInt();
+				
+		if (num%2==0) {
+			
+			System.out.println("Number "+num+ " is a Even Number");
+			
+		}
+
+	}
+
+}
---
+++ prasadJava/Helloeclipse.java
@@ -0,0 +1,11 @@
+package com.javaprograms;
+
+public class Helloeclipse {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Eclipse");
+
+	}
+
+}
