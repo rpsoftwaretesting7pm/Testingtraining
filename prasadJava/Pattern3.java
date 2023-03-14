4015cdb08e5a2f2dbcec0f64a3001520cecbcbb6
PrasadSMN <rpsoftwaretesting49@gmail.com>
2023-03-14 17:49:40 +0530

    14-03-2023 Programs

---
+++ prasadJava/Pattern3.java
@@ -0,0 +1,28 @@
+package com.javaprograms;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++)
+		{
+		  for(int j=1;j<=5;j++)
+		  {
+		      if(i==j||i==5||j==1|| j==i)
+		      {
+			  System.out.print("*");
+
+		   }else{
+		   
+		    System.out.print(" ");
+		   }
+		}
+		  System.out.println();
+	}
+}
+
+
+	}
+
+
